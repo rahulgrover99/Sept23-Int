f1f261ecf5a378a446499a6407d355635f0d282c
rahulgrover99 <rahulgrover99@gmail.com>
2023-11-03 21:59:18 +0000

    Add reading notes for polymorphism and inheritance.

--- IntroToOop/src/overriding/Zoo.java
+++ IntroToOop/src/overriding/Zoo.java
@@ -10,12 +10,12 @@ public class Zoo {
 
 
 //        sparrow.eat();
-        b1.eat();
+//        b1.eat();
 //        Bird s1 = new Sparrow();
-//        Bird s2 = new Sparrow();
-//        Bird p1 = new Penguin();
-//        p1.fly();
-//        makeBirdsFly(s1, s2, p1);
+        Bird s2 = new Sparrow();
+        Bird p1 = new Penguin();
+        p1.fly();
+        makeBirdsFly(s1, s2, p1);
     }
 
     static void makeBirdsFly(Bird... birds) {
